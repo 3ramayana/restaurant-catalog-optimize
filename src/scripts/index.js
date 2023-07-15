124b796759755bffb4a2d695ee98fc0f1138d02a
3ramayana <3ramayana@gmail.com>
2023-07-15 22:23:12 +0900

    add service worker

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
 import App from './views/app';
+import swRegister from './utils/sw-register';
 
 // Web Components
 import './components/nav-menu';
@@ -20,4 +21,5 @@ window.addEventListener('hashchange', () => {
 
 window.addEventListener('load', () => {
   app.renderPage();
+  swRegister();
 });
