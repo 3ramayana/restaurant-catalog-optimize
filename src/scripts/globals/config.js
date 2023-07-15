91e6b417afab88ee821c897d83eea9eb05a5bdb6
3ramayana <3ramayana@gmail.com>
2023-07-15 22:33:52 +0900

    add caching API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -6,6 +6,10 @@ const CONFIG = {
     'https://restaurant-api.dicoding.dev/images/medium',
   BASE_IMAGE_URL_LARGE_RESOLUTION:
     'https://restaurant-api.dicoding.dev/images/large',
+  CACHE_NAME: 'RestoCatalogue-V1',
+  DATABASE_NAME: 'resto-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'resto',
 };
 
 export default CONFIG;
--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,32 @@
-self.addEventListener('install', (event) => {
-  console.log('Installing Service Worker ...');
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
 
-  // TODO: Caching App Shell Resource
+// Daftar asset yang akan dicaching
+const assetsToCache = [
+  './',
+  './images/icons/maskable_icon.png',
+  './images/icons/maskable_icon_x48.png',
+  './images/icons/maskable_icon_x72.png',
+  './images/icons/maskable_icon_x96.png',
+  './images/icons/maskable_icon_x128.png',
+  './images/icons/maskable_icon_x192.png',
+  './images/icons/maskable_icon_x384.png',
+  './images/icons/maskable_icon_x512.png',
+  './index.html',
+  './images/icons/favicon.png',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Activating Service Worker ...');
-
-  // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
-  // TODO: Add/get fetch request to/from caches
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
