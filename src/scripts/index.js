6ccccc8f94ea921d297fa4b4a39f5c977a56b067
3ramayana <3ramayana@gmail.com>
2023-07-16 11:08:47 +0900

    optimize google fonts

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,5 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
-import '../styles/responsive.css';
+import '../styles/styles.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
 import 'lazysizes';
